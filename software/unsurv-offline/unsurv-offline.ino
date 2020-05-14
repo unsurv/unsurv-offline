2457073d859d1bcae818428538605dde599aabc1
unsurv <43016521+unsurv@users.noreply.github.com>
2020-05-14 12:57:35 +0200

    added BluetoothSerial for debugging

--- software/unsurv-offline/unsurv-offline.ino
+++ software/unsurv-offline/unsurv-offline.ino
@@ -5,41 +5,24 @@
   Date: January 3rd, 2019
   License: MIT. See license file for more information but you can
   basically do whatever you want with this code.
-
-  This example shows how to query a Ublox module for its lat/long/altitude. We also
-  turn off the NMEA output on the I2C port. This decreases the amount of I2C traffic 
-  dramatically.
-
-  Note: Long/lat are large numbers because they are * 10^7. To convert lat/long
-  to something google maps understands simply divide the numbers by 10,000,000. We 
-  do this so that we don't have to use floating point numbers.
-
-  Leave NMEA parsing behind. Now you can simply ask the module for the datums you want!
-
-  Feel like supporting open source hardware?
-  Buy a board from SparkFun!
-  ZED-F9P RTK2: https://www.sparkfun.com/products/15136
-  NEO-M8P RTK: https://www.sparkfun.com/products/15005
-  SAM-M8Q: https://www.sparkfun.com/products/15106
-
-  Hardware Connections:
-  Plug a Qwiic cable into the GPS and a BlackBoard
-  If you don't have a platform with a Qwiic connection use the SparkFun Qwiic Breadboard Jumper (https://www.sparkfun.com/products/14425)
-  Open the serial monitor at 115200 baud to see the output
 */
 
 #include <Wire.h> //Needed for I2C to GPS
+#include "BluetoothSerial.h"
+
 
 #include "SparkFun_Ublox_Arduino_Library.h" //http://librarymanager/All#SparkFun_Ublox_GPS
 SFE_UBLOX_GPS myGPS;
-
+BluetoothSerial ESP_BT;
 long lastTime = 0; //Simple local timer. Limits amount if I2C traffic to Ublox module.
 
 void setup()
 {
-  Serial.begin(115200);
-  while (!Serial); //Wait for user to open terminal
+  Serial.begin(9600);
   Serial.println("SparkFun Ublox Example");
+  
+  ESP_BT.begin("test");
+
 
   Wire.begin();
 
@@ -64,21 +47,34 @@ void loop()
     long latitude = myGPS.getLatitude();
     Serial.print(F("Lat: "));
     Serial.print(latitude);
+    ESP_BT.print(F("Lat: "));
+    ESP_BT.print(latitude);
 
     long longitude = myGPS.getLongitude();
-    Serial.print(F(" Long: "));
+    Serial.print(F(" Lon: "));
     Serial.print(longitude);
-    Serial.print(F(" (degrees * 10^-7)"));
+
+    ESP_BT.print(F(" Lon: "));
+    ESP_BT.print(longitude);
 
     long altitude = myGPS.getAltitude();
     Serial.print(F(" Alt: "));
     Serial.print(altitude);
     Serial.print(F(" (mm)"));
 
+    ESP_BT.print(F(" Alt: "));
+    ESP_BT.print(altitude);
+
     byte SIV = myGPS.getSIV();
     Serial.print(F(" SIV: "));
     Serial.print(SIV);
 
+    ESP_BT.print(F(" SIV: "));
+    ESP_BT.print(SIV);
+
     Serial.println();
+    ESP_BT.println();
+
+
   }
 }
